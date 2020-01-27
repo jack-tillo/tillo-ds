9b86e2be73762df0f0d865dba9b1a775f9548bca
Jack Chapman <jack@reward.cloud>
2020-01-27 15:35:50 +0000

    Add core and README

---
+++ packages/Core/src/index.js
@@ -0,0 +1,8 @@
+import Button from '@tillo/button';
+import TextInput from '@tillo/text-input';
+export default {
+    TilloButton: Button,
+    TilloTextInput: TextInput
+}
+export { Button as TilloButton }
+export { TextInput as TilloTextInput }
