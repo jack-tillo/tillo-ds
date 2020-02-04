d5ae0269a1c78cb88cc6e2149ed24c44829235c2
Jack Chapman <jack@reward.cloud>
2020-02-04 14:54:17 +0000

    Build core to include dropdown

--- packages/Core/src/index.js
+++ packages/Core/src/index.js
@@ -1,11 +1,14 @@
 import Button from '@tillo-ds/button';
 import TextInput from '@tillo-ds/text-input';
 import Title from '@tillo-ds/title';
+import Dropdown from '@tillo-ds/dropdown';
 export default {
     TilloButton: Button,
     TilloTextInput: TextInput,
-    TilloTitle: Title
+    TilloTitle: Title,
+    TilloDropdown: Dropdown
 }
 export { Button as TilloButton }
 export { TextInput as TilloTextInput }
 export { Title as TilloTitle }
+export { Dropdown as TilloDropdown }
